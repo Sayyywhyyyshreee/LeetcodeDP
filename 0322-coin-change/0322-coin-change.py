12982cea45c3e7d62b02e9cfaae3e15825bd3968
Sayyywhyyyshreee <saishreesmaranikapanda20@gmail.com>
2026-07-16 23:36:15 +0530

    Time: 1135 ms (9.05%), Space: 23.5 MB (22.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,24 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        dp = [[-1] * (amount+1) for _ in range(n)]
-        def f(ind,t):
+        dp = [[0] * (amount+1) for _ in range(n)]
+       
             
-           
-            if ind == 0:
+        for t in range(0,amount+1):
                 if t % coins[0] == 0:
-                    return t//coins[0]
+                    dp[0][t] = t//coins[0]
                 else:
-                    return float('inf')
-            if dp[ind][t] != -1:
-                return dp[ind][t]
-            nontake = 0 + f(ind-1,t)
-            take = float('inf')
-            if coins[ind] <= t:
-                take = 1 + f(ind,t-coins[ind])
-            dp[ind][t] = min(take,nontake)
-            return dp[ind][t] 
-        ans = f(n - 1, amount)
+                    dp[0][t] = float('inf')
+        for i in range(1,n):
+            for t in range(0,amount+1):
+                nontake = 0 + dp[i-1][t]
+                take = float('inf')
+                if coins[i] <= t:
+                    take = 1 + dp[i][t-coins[i]]
+                dp[i][t] = min(take,nontake)
+                
+        ans = dp[n - 1] [amount]
         return ans if ans != float('inf') else -1
             
 
