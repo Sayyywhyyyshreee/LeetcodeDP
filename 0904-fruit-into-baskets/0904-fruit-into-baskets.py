d7e87a6454fc176bfcdce904d30e57ea5f7920ff
Sayyywhyyyshreee <saishreesmaranikapanda20@gmail.com>
2026-08-05 10:41:35 +0530

    Time: 198 ms (27.64%), Space: 25.9 MB (34.14%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,22 +1,19 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        l=0
+        maxi=0
         mpp = {}
-        l = 0
-        maxi = 0
         for r in range(len(fruits)):
             if fruits[r] in mpp:
-                mpp[fruits[r]] +=1
+                mpp[fruits[r]] += 1
             else:
                 mpp[fruits[r]] = 1
             while len(mpp) > 2:
                 mpp[fruits[l]] -= 1
-                if mpp[fruits[l]] == 0:
+                if mpp[fruits[l]] == 0: 
                     del mpp[fruits[l]]
-                l+= 1
+                l += 1
             maxi = max(maxi,r-l+1)
         return maxi
-            
-            
-                
 
         
